73429d34324ed2eb2b43b9a315e8fd8ae41be2e4
Sebastian Rincon <serincont@gmail.com>
2022-11-21 16:49:22 -0500

    add controllers

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,3 +2,6 @@ export * from './ping.controller';
 export * from './product-company.controller';
 export * from './company-product.controller';
 export * from './product-category.controller';
+export * from './company.controller';
+export * from './category.controller';
+export * from './product.controller';
